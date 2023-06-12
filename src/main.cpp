1fd322bc9ead4e9bd06b66a6d52373b458527e73
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-06-12 13:23:25 +0700

    Update the algorithm

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <ctype.h>
+/*Read String from Serial Monitor*/
+
+String myOldString;
+String myString;
+String ctr = "Please enter the LED you want to blink: ";
+String ctr2 = "Please enter string not other data: ";
+int redPin = 11;
+int greenPin = 10;
+int yellowPin = 9;
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+//Convert User input to lowercase
+String toLower(String str) {
+  String tmp ="";
+  for(int i = 0; (unsigned) i < str.length(); i++) {
+    if(islower(str.charAt(i))) {
+      tmp += str[i];
+      continue;
+    }
+    else if(isupper(str.charAt(i))) {
+      char x = toLowerCase(str[i]);
+      tmp += x;
+    }
+  }
+  return tmp;
+}
+
+void loop() {
+  Serial.print(ctr);
+  while(Serial.available() == 0) {
+
+  }
+  myOldString = Serial.readString(); // get String from the serial monitor
+  myString = toLower(myOldString);
+  Serial.println(myString);
+  Serial.println();
+  while(myString != "red" && myString != "green" && myString != "yellow") {
+    Serial.println();
+    Serial.print(ctr2);
+    while(Serial.available() == 0) {
+
+    }
+    myOldString = Serial.readString();
+    myString = toLower(myOldString);
+  }
+
+  if(myString == "red") {
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(yellowPin, 0);
+  }
+  else if(myString == "green") {
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(yellowPin, 0);
+  }
+  else if(myString == "yellow") {
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(yellowPin, 255);
+  }
+  Serial.println();
+}
